03a9de19512bcbfcefc9a176d4b104106dfe2988
Dani <daniel.varlan20@gmail.com>
2022-11-28 16:41:14 +0100

    Updated Bingo Command
    Added experimental feature -> Checks all the player inventories (in 1 team) for the required items

--- src/main/java/bingotests/bingotests/commands/BingoCommand.java
+++ src/main/java/bingotests/bingotests/commands/BingoCommand.java
@@ -1,6 +1,8 @@
 package bingotests.bingotests.commands;
 
 import bingotests.bingotests.BingoTests;
+import bingotests.bingotests.team.Team;
+import bingotests.bingotests.team.TeamHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,8 +11,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+
 
 public class BingoCommand implements CommandExecutor {
+
+    TeamHandler handler = BingoTests.getMain().getHandler();
+    ArrayList<ItemStack> teaminv = new ArrayList<>();
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         // Checks if the user (or maybe the team) has all the Bingoboard items
@@ -20,6 +28,31 @@ public class BingoCommand implements CommandExecutor {
         }
 
         Player player = (Player) sender;
+
+        // TODO: This feature still requires testing!
+        // Creates a teaminv out of all the team members inventories & checks if all items were obtained
+        if(!(handler.getTeamlist().isEmpty())) {
+            for(Team t : handler.getTeamlist()) {
+                if (t.getMembers().contains(player)) {
+                    for (Player member : t.getMembers()) {
+                        for(int i = 0; i <= member.getInventory().getSize(); ++i) {
+                            teaminv.add(member.getInventory().getItem(i));
+                        }
+                   }
+                }
+            }
+            for(ItemStack i : BingoTests.getMain().getBingo()) {
+                if(!(teaminv.contains(i))) {
+                    player.sendMessage("Your team did not obtain every item!");
+                    teaminv.clear();
+                    return false;
+                }
+            }
+            player.sendMessage("Your team obtained every item!");
+            return false;
+        }
+
+
         for(ItemStack i : BingoTests.getMain().getBingo()) {
             //DEBUG player.sendMessage(i.toString());
             if(!(player.getInventory().contains(i))) {
